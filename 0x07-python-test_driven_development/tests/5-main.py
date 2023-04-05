eba7564a8b174daa3b3fa6977cf64668dd4bff73
Amr Sayed <114165022+AmrSayed0@users.noreply.github.com>
2023-04-05 07:52:14 +0200

    Delete 0x07-python-test_driven_development directory

--- 0x07-python-test_driven_development/tests/5-main.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/python3
-
-from text_indentation import text_indentation
-
-text = """This is a sample text. It includes multiple sentences. Each sentence ends with a separator: . or ? or :"""
-text_indentation(text)
