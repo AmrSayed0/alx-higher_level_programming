6baa92033522886418bb53f6531ed84a38791555
AmrSayed0 <amracademycode@gmail.com>
2023-04-05 03:41:01 +0200

    main 5

---
+++ 0x07-python-test_driven_development/tests/5-main.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+
+from text_indentation import text_indentation
+
+text = """This is a sample text. It includes multiple sentences. Each sentence ends with a separator: . or ? or :"""
+text_indentation(text)
