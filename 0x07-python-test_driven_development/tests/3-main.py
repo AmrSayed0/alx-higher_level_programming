aa83cdba1985e7f7c685d89f9d732214f549dc78
AmrSayed0 <amracademycode@gmail.com>
2023-04-05 03:31:15 +0200

    3-main

---
+++ 0x07-python-test_driven_development/tests/3-main.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+from say_name import say_my_name
+
+def main():
+    # Call the function with first and last name
+    say_my_name("John", "Doe")
+    # Call the function with only first name
+    say_my_name("Jane")
+
+if __name__ == '__main__':
+    main()
