b34c8c89f62d16ea07957764c4ec8b0c82dfbbbe
Amr Sayed <114165022+AmrSayed0@users.noreply.github.com>
2023-06-12 23:58:38 +0300

    Delete 0x12-javascript-warm_up directory

--- 0x12-javascript-warm_up/2-arguments.js
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/node
-const myArgument = process.argv.length;
-console.log(myArgument === 2 ? 'No argument' : myArgument === 3 ? 'Argument found' : 'Arguments found');
--- 0x12-javascript-warm_up/5-to_integer.js
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/node
-const myNum = Math.floor(Number(process.argv[2]));
-console.log(is NaN(myNum) ? 'Not a number' : `My number: ${myNum}`);
