f34ee46f524bdde7746c1d5f23ad2607696b41e5
AmrSayed0 <amracademycode@gmail.com>
2023-06-30 11:49:44 -0700

    [Updated] Fixed a bug in check 6

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# This script makes a request to the endpoint 0.0.0.0:5000/catch_me and sends a PUT request with specific data and headers.
+# This script makes a request to 0.0.0.0:5000/catch_me that gets the message "You got me!".
 curl -s -X PUT -d "user_id=98" -H "Origin: You got me!" -L 0.0.0.0:5000/catch_me
